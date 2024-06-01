f781ca7e1375b2ae6b7fecfff64824a0bc28a346
Pabil <pabil@d207-023-182-050.wireless.sfu.ca>
2024-05-31 17:45:15 -0700

    worte requests

--- Backend/data_processing.py
+++ Backend/data_processing.py
@@ -1,3 +1,11 @@
 # TODO Fetch and prcoess protein data via uniport api 
 import requests
+import urllib.parse
 
+api_Url = ("https://rest.uniprot.org/uniprotkb/search?query=")
+
+def create_uniprot_url():
+    organism_name = input("Please enter the name of the organism you want ")
+
+
+    return url
